52a0b3317a28ee1f0adb994bd83f92c4d26e9ba6
emmanuelleygit <131384092+emmanuelleygit@users.noreply.github.com>
2023-04-21 11:58:49 +0200

    Delete server directory
    minor changes

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-// secret phrase for authentification
-// if you want to generate a new one on
-// each new server, change this to a random
-// string
-module.exports = {
-    secret: 'pixanoisamazing'
-  };
